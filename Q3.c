e406ffb96eff475306af0276f618ec5c1c45624c
Pranav Pawar <148087983+prnvpwr2612@users.noreply.github.com>
2023-12-09 21:59:46 +0530

    Q3.c

--- Q3.c
+++ Q3.c
@@ -1,81 +1,61 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <pthread.h>
+#include<stdlib.h>
+#include<conio.h>
+#include<stdio.h>
 
-#define BUFFER_SIZE 5
-#define MAX_ITEMS 5
+int mutex = 1;
+int full = 0;
+int empty = 5;
+int data = 0;
 
-int buffer[BUFFER_SIZE];
-int in = 0;
-int out = 0;
-int produced_count = 0;
-int consumed_count = 0;
-
-pthread_mutex_t mutex;
-pthread_cond_t full;
-pthread_cond_t empty;
-
-void* producer(void* arg) {
-   int item = 1;
-
-   while (produced_count < MAX_ITEMS) {
-      pthread_mutex_lock(&mutex);
-
-      while (((in + 1) % BUFFER_SIZE) == out) {
-         pthread_cond_wait(&empty, &mutex);
-      }
-
-      buffer[in] = item;
-      printf("Produced: %d", item);
-      item++;
-      in = (in + 1) % BUFFER_SIZE;
-
-      produced_count++;
-
-      pthread_cond_signal(&full);
-      pthread_mutex_unlock(&mutex);
-   }
-
-   pthread_exit(NULL);
+void wait(int *S) {
+    while(*S<=0);
+    (*S)--;
 }
 
-void* consumer(void* arg) {
-   while (consumed_count < MAX_ITEMS) {
-      pthread_mutex_lock(&mutex);
+void signal(int *S) {
+    (*S)++;
+}
 
-      while (in == out) {
-         pthread_cond_wait(&full, &mutex);
-      }
+void producer() {
+    wait(&empty);
+    wait(&mutex);
+    data++;
+    signal(&mutex);
+    signal(&full);
+    printf("\nProduced item %d",data);
+}
 
-      int item = buffer[out];
-      printf("Consumed: %d", item);
-      out = (out + 1) % BUFFER_SIZE;
-
-      consumed_count++;
-
-      pthread_cond_signal(&empty);
-      pthread_mutex_unlock(&mutex);
-   }
-
-   pthread_exit(NULL);
+void consumer() {
+    wait(&full);
+    wait(&mutex);
+    printf("\nConsumed Item %d",data);
+    data--;
+    signal(&mutex);
+    signal(&empty);
 }
 
 int main() {
-   pthread_t producerThread, consumerThread;
+    int i, n;
+    //clrscr();
+    while(1) {
+        printf("\n1.) Enter 1 for Producer\n2.) Enter 2 for Consumer\n3.) Enter 3 to Exit\n");
+        scanf("%d", &n);
+        switch(n) {
+        case 1: if(mutex==1 && empty!=0)
+                    producer();
+                else
+                    printf("Buffer is full");
+                break;
+            
+        case 2: if(mutex==1 && full!=0)
+                    consumer();
+                else
+                    printf("Buffer is empty");
+                break;
 
-   pthread_mutex_init(&mutex, NULL);
-   pthread_cond_init(&full, NULL);
-   pthread_cond_init(&empty, NULL);
-
-   pthread_create(&producerThread, NULL, producer, NULL);
-   pthread_create(&consumerThread, NULL, consumer, NULL);
-
-   pthread_join(producerThread, NULL);
-   pthread_join(consumerThread, NULL);
-
-   pthread_mutex_destroy(&mutex);
-   pthread_cond_destroy(&full);
-   pthread_cond_destroy(&empty);
-
-   return 0;
+        case 3: exit(0);
+                break;
+        }
+    }
+    getch();
 }
